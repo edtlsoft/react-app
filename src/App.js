1f4db2dceb3cd2baf984bee7ac4219f6549031af
Edtlsoft <edwardlopez282@gmail.com>
2020-06-25 23:14:06 -0500

    State in React

--- src/App.js
+++ src/App.js
@@ -2,12 +2,41 @@ import React from 'react';
 //import HelloWorld from './HelloWorld';
 import './App.css';
 
-function HelloWorld(props) {
-  console.log(props)
-  return (
-    <div id="hello-world">{ props.texto }</div>
-  )
+// function HelloWorld(props) {
+//   console.log(props)
+//   return (
+//     <div id="hello-world">{ props.texto }</div>
+//   )
+// }
+
+class HelloWorld extends React.Component {
+  state = {
+    show: true
+  }
+
+  toggleShow() {
+    this.setState({show: !this.state.show})
+  }
+
+  render() {
+    if( !this.state.show ) {
+      return (
+        <h1>
+          There are not elements to show
+          <button onClick={() => this.toggleShow()}>Toggle Show</button>
+        </h1>
+      )
+    }
+
+    return (
+      <div id="hello-world">
+        { this.props.texto }
+        <button onClick={() => this.toggleShow()}>Toggle Show</button>
+      </div>
+    )
+  }
 }
+
 
 function App() {
   return (
