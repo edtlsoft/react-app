147a1b289a0011d8a93e0baa935c9512ba2bd3c9
Edtlsoft <edwardlopez282@gmail.com>
2020-06-25 22:48:47 -0500

    Props in React

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import React from 'react';
+//import HelloWorld from './HelloWorld';
+import './App.css';
+
+function HelloWorld(props) {
+  console.log(props)
+  return (
+    <div id="hello-world">{ props.texto }</div>
+  )
+}
 
 function App() {
   return (
     <div className="App">
-      Hello World
+      This is my component
+      <HelloWorld id="uno" texto="Hello World!" />
+      <HelloWorld texto="Hello Continent!" />
+      <HelloWorld texto="Hello Country!" />
     </div>
   );
 }
