d1cfce905555b7359bfcc58c0672d53f8cec243f
Edtlsoft <edwardlopez282@gmail.com>
2020-06-26 21:01:35 -0500

    Adding styles in ReactJs

--- src/App.js
+++ src/App.js
@@ -3,8 +3,6 @@ import './App.css';
 import Tasks from './components/Tasks'
 import tasks from './data/Tasks'
 
-console.log(tasks)
-
 function App() {
   return (
     <div className="App">
--- src/components/Task.js
+++ src/components/Task.js
@@ -1,17 +1,30 @@
 import React from 'react'
+import './Task.css'
 
 class Tasks extends React.Component {
 	render() {
 		const task = this.props.task
 
 		return (
-			<p>
+			<p className="red">
 				{task.id} - {task.title} - {task.description}
 				<input type="checkbox" />
-				<button>X</button>
+				<button style={btnDelete}>
+					X
+				</button>
 			</p>
 		)
 	}
+}
+
+const btnDelete = {
+	fontSize: '18px',
+	background: '#ea2027',
+	color: '#fff',
+	border: 'none',
+	padding: '10px 15px',
+	borderRadius: '50%',
+	cursor: 'pointer',
 }
 
 export default Tasks
