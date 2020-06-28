018e94c2dedbf4123d42fc3196d80acb5098bcdd
Edtlsoft <edwardlopez282@gmail.com>
2020-06-28 13:16:21 -0500

    Implement React Router DOM

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import {BrowserRouter as Router, Route, Link} from 'react-router-dom'
+
 import './App.css';
 
 import Tasks from './components/Tasks'
@@ -46,14 +48,45 @@ export default class App extends React.Component {
 	render() {
 		return (
 			<div className="App container">
-				<TaskForm addTask={this.addTask} />
+				<Router>
+					<nav className="navbar navbar-expand-lg navbar-light bg-light">
+					  <a className="navbar-brand" href="#">ReactApp</a>
+					  <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+					    <span className="navbar-toggler-icon"></span>
+					  </button>
 
-				<Tasks
-					tasks={this.state.tasks}
-					checkDone={this.checkDone}
-					deleteTask={this.deleteTask} />
+					  <div className="collapse navbar-collapse" id="navbarSupportedContent">
+					    <ul className="navbar-nav mr-auto">
+					      <li className="nav-item">
+					        <Link to="/" className="nav-link" activeClassName="active">
+					        	TaskList
+					        </Link>
+					      </li>
+					      <li className="nav-item">
+					        <Link to="/posts" className="nav-link" activeClassName="active">
+					        	Posts
+					        </Link>
+					      </li>
+					    </ul>
+					  </div>
+					</nav>
 
-				<Posts />
+					<Route exact path="/" render={() => {
+						return (
+							<div>
+								<TaskForm addTask={this.addTask} />
+
+								<Tasks
+									tasks={this.state.tasks}
+									checkDone={this.checkDone}
+									deleteTask={this.deleteTask} />
+							</div>
+
+						)
+					}} />
+
+					<Route path="/posts" component={Posts} />
+				</Router>
 			</div>
 		)
 	}
